9045b14c68f7625605b66f6664212fe68782c101
amolu <amolujagare@gmail.com>
2021-11-24 08:57:40 +0530

    Initial commit

---
+++ src/SeleniumDemo.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDemo {
+
+
+    public static void main(String[] args) {
+
+
+        System.setProperty("webdriver.chrome.driver","Drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("http://faccebook.com");
+
+
+    }
+}
