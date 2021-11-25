a08c5a4b209f5f98d676737f7107703ead9bcd6d
amolu <amolujagare@gmail.com>
2021-11-25 08:59:03 +0530

    selenium

--- src/SeleniumDemo.java
+++ src/SeleniumDemo.java
@@ -1,5 +1,8 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class SeleniumDemo {
 
@@ -7,12 +10,30 @@ public class SeleniumDemo {
     public static void main(String[] args) {
 
 
-        System.setProperty("webdriver.chrome.driver","Drivers/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+       /* System.setProperty("webdriver.chrome.driver","Drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();*/
+
+        // firefox
+      /*  System.setProperty("webdriver.gecko.driver","Drivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver(); // upcasting*/
+
+       /* System.setProperty("webdriver.opera.driver","Drivers/operadriver.exe");
+        WebDriver driver = new OperaDriver();*/
+
+        System.setProperty("webdriver.edge.driver","Drivers/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+
 
         driver.manage().window().maximize();
+        driver.get("http://naukri.com");
 
-        driver.get("http://faccebook.com");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getPageSource());
+
+        //driver.close(); // closes the current window
+
+        driver.quit(); // closes all the windows opened by the driver
 
 
     }
